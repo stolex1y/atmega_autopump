445dc32c163687011d1a46a0210324f5b0d385ec
Alexey Filimonov <stolexiy@mail.ru>
2022-01-13 21:49:45 +0300

    Убраны лишние комментарии в coordinates

--- src/coordinates.h
+++ src/coordinates.h
@@ -14,10 +14,4 @@ struct coordinates coordinates_create(coord_type x, coord_type y);
 
 struct coordinates coordinates_zeros();
 
-//void coordinates_set_x(struct coordinates* coordinates, uint8_t x);
-//void coordinates_set_y(struct coordinates* coordinates, uint8_t y);
-//void coordinates_set(struct coordinates* coordinates, uint8_t x, uint8_t y);
-//void coordinates_get_x(struct coordinates* coordinates);
-//void coordinates_get_y(struct coordinates* coordinates);
-
 #endif //ARDUINO_AUTOPUMP_COORDINATES_H
